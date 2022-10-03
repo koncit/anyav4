9ee3335ec191890d583c040bb9b27784a3cb011a
koncit <tnnckl1553@gmail.com>
2022-10-03 14:00:17 +0700

    anyav4

---
+++ README.md
@@ -0,0 +1,33 @@
+<div align="center">
+
+<img src="https://telegra.ph/file/c03ec512d671c3e26727b.jpg" alt="Anya Md-V3" width="300" />
+
+</p>
+
+<h1 align="center">KONCIT OFC</h1>
+
+>
+
+>
+
+>
+
+</div>
+
+<p align="center">
+
+  <a href="https://github.com/koncit"><img title="GitHub" src="https://img.shields.io/badge/Github-ramlaidi.svg?style=for-the-badge&logo=github" /></a>
+
+  <a href="httts://instagram.com/k0nc1t.store"><img title="Instagram " src="https://img.shields.io/badge/Instagram-Hyzerr.svg?style=for-the-badge&logo=instagram" /></a>
+
+  <a href="https://www.youtube.com/channel/UClfUAjgtQ19vGnUbkQxTuAg"><img title="Youtube" src="https://img.shields.io/badge/Youtube-Hyzerr.svg?style=for-the-badge&logo=youtube" /></a>
+
+  <h4 align="center">
+
+
+
+  <a href="https://wa.me/6281364444859?text=bang mau nanya">Klik disini untuk whatsapp Koncit </a>
+
+</h4>
+
+</p>
---
+++ config.js
@@ -0,0 +1,54 @@
+/**
+   * Create By KoncitOfc 
+   * Contact Me on wa.me/621364444859
+   * Follow https://github.com/koncit
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['6281364444859']
+global.ownernomer = "6281364444859"
+global.premium = ['6281364444859']
+global.packname = 'Anya-Bot'
+global.author = '© Koncit'
+global.sessionName = 'knct'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: 'Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 25
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+global.vaze = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
